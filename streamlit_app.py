081f219670ec9b3ae87a3e68f4314a286e220243
Amandajxl <amandaljx730@gmail.com>
2025-11-13 15:04:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 sfi boy")
+st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
